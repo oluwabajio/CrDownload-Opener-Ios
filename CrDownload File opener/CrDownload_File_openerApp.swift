4e1c07537363ac22999536c70c88dd6732fa85bd
oluwabajio <ismailosunlana@yahoo.com>
2023-04-16 06:06:52 +0100

    Initial Commit

---
+++ CrDownload File opener/CrDownload_File_openerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrDownload_File_openerApp.swift
+//  CrDownload File opener
+//
+//  Created by oluwabajio on 16/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CrDownload_File_openerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
